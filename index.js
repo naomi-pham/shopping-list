9bc99cc6e03e4575482ed71e3fbafa134e2a8754
Pham Bao Nguyen <75587231+baonguyenpham@users.noreply.github.com>
2022-08-17 12:33:38 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ let myList = []
 let myPrice = []
 let oldList = []
 let budget = ""
+let warningInBudget = [
+    "Hooray! You're still within budget. 😍", 
+    "Woo-hoo! Happy shopping! 🎉", 
+    "Whoopee! You're still within budget. 🥳"
+]
+let warningOutBudget = [
+    "Oh no! You've run out of budget. 😭", 
+]
 
 //My budget and sum
 const budgetEl = document.getElementById("budget-el")
@@ -31,48 +39,25 @@ const captureBtn = document.getElementById("capture-btn")
 
 //LocalStorage
 let listFromLocalStorage = JSON.parse( localStorage.getItem("myList") )
-//console.log(listFromLocalStorage)
 let priceFromLocalStorage = JSON.parse(localStorage.getItem("myPrice"))
-//console.log(priceFromLocalStorage)
 let budgetFromLocalStorage = JSON.parse(localStorage.getItem("Budget"))
-//console.log(typeof budgetFromLocalStorage)
 let sumFromLocalStorage = localStorage.getItem("Sum")
-//console.log(typeof sumFromLocalStorage)
 
 //close-btn and remove child
 const closeBtn = document.getElementsByClassName("close")
-
 
 //warning
 const warningEl = document.getElementById("warning-el")
 
 
-// render localStorage
-
-/*
-
-if (priceFromLocalStorage) {
-    let myPrice = priceFromLocalStorage
-    renderPrice(myPrice)
-  }
-  
-if (listFromLocalStorage) {
-   let myList = listFromLocalStorage
-   renderList(myList)
- }
- 
- */
- 
-
+//render budget and sum from Local Storage
 if (budgetFromLocalStorage) {
   let budget = budgetFromLocalStorage
   budgetEl.textContent = ""
   budgetEl.textContent = "New budget: "
   prevBudgetEl.textContent = "Your budget last time:" + " $" + budget
   prevBudgetEl.style.display = "block"
-  
 }
-
 
 if (sumFromLocalStorage) {
   let sum = sumFromLocalStorage
@@ -98,32 +83,36 @@ captureBtn.addEventListener("click", function() {
 })
 
 
-//onclick submitBtn
+//onclick submitBtn, display Budget
 submitBtn.addEventListener("click", function() {
-    inputContainer.style.display = "block"
-    inputBtn.style.display = "block" 
+    if(myBudget.value) {
+        inputContainer.style.display = "block"
+        inputBtn.style.display = "block" 
+        
+        myBudget.style.display = "none"
+        submitBtn.style.display = "none"
+        prevBudgetEl.style.display = "none"
+        prevSumEl.style.display = "none"
+        
+        myList = []
+        myPrice = []
+        
+        renderList(myList)
+        renderPrice(myPrice)
+        
+        budgetEl.textContent = "My budget:" + " $" + myBudget.value
+        
+        
+        let x = myBudget.value
+        localStorage.setItem("Budget", JSON.stringify(x))
+    } else {
+        alert("Please enter your budget")
+    }
     
-    myBudget.style.display = "none"
-    submitBtn.style.display = "none"
-    prevBudgetEl.style.display = "none"
-    prevSumEl.style.display = "none"
-    
-    myList = []
-    myPrice = []
-    
-    renderList(myList)
-    renderPrice(myPrice)
-    
-    budgetEl.textContent = "My budget:" + " $" + myBudget.value
-    
-    
-    let x = myBudget.value
-    localStorage.setItem("Budget", JSON.stringify(x))
-    //console.log(localStorage.getItem("Budget"))
   })
 
 
-//Function renderList
+//renderList
 function renderList(list) {
     let listItems = ""
     for (let i = 0; i < list.length; i++) {
@@ -137,6 +126,7 @@ function renderList(list) {
     }
     listItem.innerHTML = listItems
     
+    //onclick clostBtn
     for (let i=0; i < closeBtn.length; i++) {
       closeBtn[i].onclick = function() {
         this.parentNode.remove()
@@ -151,9 +141,7 @@ function renderList(list) {
 }
 
 
-
-
-//Function renderPrice
+//renderPrice
 function renderPrice(price) {
     let priceItems = ""
     for (let i = 0; i < price.length; i++) {
@@ -189,60 +177,51 @@ clearBtn.addEventListener("click", function() {
 
 //onclick inputBtn
 inputBtn.addEventListener("click", function() {
-    myList.push(inputEl.value)
-    inputEl.value = ""
-    myPrice.push(priceEl.value)       
-    priceEl.value = ""
-    
-    localStorage.setItem("myList", JSON.stringify(myList))
-    localStorage.setItem("myPrice", JSON.stringify(myPrice))
+    if (inputEl.value && priceEl.value) {
+        myList.push(inputEl.value)
+        inputEl.value = ""
+        myPrice.push(priceEl.value)       
+        priceEl.value = ""
+        
+        localStorage.setItem("myList", JSON.stringify(myList))
+        localStorage.setItem("myPrice", JSON.stringify(myPrice))
 
+        
+        renderList(myList) 
+        renderPrice(myPrice)
+        sumPrice(myPrice)        
+    } else {
+        alert("Please enter item/price.")
+    }
     
-    renderList(myList) 
-    renderPrice(myPrice)
-    sumPrice(myPrice)
 })
 
-
-//function sumPrice
-
-/* function sum(numbers){
-  var x = numbers.reduce(function(prev,curr){
-    return curr + prev;
-  },0);
-  return x;
-}
-
-alert(sum([1,2,3]));
-alert(sum([])); */
-
-/* var sum = array.reduce(function(a, b){
-        return a + b;
-    }, 0); */
-
-
-
+//sumPrice
 function sumPrice(price) {
     let totalPrice = 0
     sumEl.textContent = "Sum:" + " $" + "0"
     warningEl.textContent = ""
     for (i = 0; i < price.length; i++) {
         totalPrice += Number(price[i])
-        sumEl.textContent = "Sum:" + " $" + totalPrice
+        sumEl.textContent = "Sum:" + " $" + (totalPrice.toFixed(2))
         console.log(totalPrice)
               
         let y = totalPrice
         localStorage.setItem("Sum",y.toString())
         if (totalPrice <= Number(myBudget.value)) {
-            warningEl.textContent = "You're within budget. Happy shopping! 😍"
+            let randomText = Math.floor(Math.random()*warningInBudget.length)
+            warningEl.textContent = warningInBudget[randomText]
+            sumEl.style.color = "#00A300"
         } else {
-            warningEl.textContent = "Oh no! You're out of budget. 😭"
+            let randomText2 = Math.floor(Math.random()*warningOutBudget.length)
+            warningEl.textContent = warningOutBudget[randomText2]
+            sumEl.style.color = "red"
         }
     }
 } 
 
 
-
+//onclick listItem, strikethrough & check
 listItem.addEventListener("click", function(ev) {
   if(ev.target.tagName==="LI")
   ev.target.classList.toggle("checked")
@@ -261,3 +240,41 @@ priceEl.addEventListener("keypress", function(event) {
         document.getElementById("input-btn").click()
     }
 })
+
+
+
+
+/*
+
+// render localStorage
+
+if (priceFromLocalStorage) {
+    let myPrice = priceFromLocalStorage
+    renderPrice(myPrice)
+  }
+  
+if (listFromLocalStorage) {
+   let myList = listFromLocalStorage
+   renderList(myList)
+ }
+ 
+ */
+
+//function sumPrice
+
+/* function sum(numbers){
+  var x = numbers.reduce(function(prev,curr){
+    return curr + prev;
+  },0);
+  return x;
+}
+
+alert(sum([1,2,3]));
+alert(sum([])); */
+
+/* var sum = array.reduce(function(a, b){
+        return a + b;
+    }, 0); */
+
+
+ 
